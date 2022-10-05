38a81b966e06fc3a4e925efa85c8b6a281ea2ee0
cocodori <moradulee@gmail.com>
2022-10-05 09:38:14 +0900

    CustomAuthenticationProvider 작성

--- ssia-ch2-ex1/src/main/java/com/security/ssia/config/ProjectConfig.java
+++ ssia-ch2-ex1/src/main/java/com/security/ssia/config/ProjectConfig.java
@@ -1,5 +1,6 @@
 package com.security.ssia.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -21,12 +22,21 @@ import javax.sql.DataSource;
 @EnableWebSecurity
 public class ProjectConfig {
 
+    private final CustomAuthenticationProvider authenticationProvider;
+
+    public ProjectConfig(CustomAuthenticationProvider authenticationProvider) {
+        this.authenticationProvider = authenticationProvider;
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.httpBasic();
+
         http.authorizeRequests()
                 .anyRequest()
                 .authenticated();
+
+        http.authenticationProvider(authenticationProvider);
 
         return http.build();
     }
